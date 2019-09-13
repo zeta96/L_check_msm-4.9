570b7910f7934a12384898dbf96f85f6584e721b
Angelo G. Del Regno <kholk11@gmail.com>
2019-09-13 14:59:05 +0200

    media: msm: camera_v2: Sanitize some probe failures
    Lots of places in this code have probe failures that are badly
    managed, producing a permanent driver failure even in cases in
    which we can effectively just defer and succeed probing later.
    
    Sanitize the immediate ones and leave the harder ones for
    later, as these will need a bit of research before doing.
    
    [ edited: Fix no member]

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -1324,7 +1324,12 @@ static int32_t msm_flash_platform_probe(struct platform_device *pdev)
 	media_entity_pads_init(&flash_ctrl->msm_sd.sd.entity, 0, NULL);
 	flash_ctrl->msm_sd.sd.entity.group_id = MSM_CAMERA_SUBDEV_FLASH;
 	flash_ctrl->msm_sd.close_seq = MSM_SD_CLOSE_2ND_CATEGORY | 0x1;
-	msm_sd_register(&flash_ctrl->msm_sd);
+
+	rc = msm_sd_register(&flash_ctrl->msm_sd);
+	if (rc) {
+		kfree(flash_ctrl);
+		return rc;
+	}
 
 	CDBG("%s:%d flash sd name = %s", __func__, __LINE__,
 		flash_ctrl->msm_sd.sd.entity.name);
