088b21a30a71c521137538609ae438494bc3aad1
zeta96 <ahmad.zainul857@gmail.com>
2020-07-19 07:45:30 +0000

    Remove tune sched feature
    Revert "sched: Reduce the opportunity for sleepers to preempt"
    
    This reverts commit 3fbce8fb5a72adcc86578aa25e966abb2a771b57.
    
    Revert "sched: Increase the time a task is considered cache-hot"
    
    This reverts commit 2a7c0008fb49702cb215f8e8fda20d5531afbd48.
    
    Revert "sched: Ideally use 10ms scheduling periods"
    
    This reverts commit 10dc02590634741b49992b8b3f22a584e6fb6f12.
    
    Revert "sched: Queue non-local tasks on a cache sharing CPU"
    
    This reverts commit 9a103698ec736cec3c35997b04017a890ad8c82d.
    
    Revert "sched: Do not reduce perceived CPU capacity while idle"
    
    This reverts commit 12e398eed98eaa39e9317367572ab087fc17e810.
    
    Revert "sched: Do not always skip yielding tasks"
    
    This reverts commit d1f8e3e3ec1b106879d8a94dedc87d5bf1dbb585.
    
    Revert "sched: Enable NEXT_BUDDY for better cache locality"
    
    This reverts commit ca2f27812fbd8590249a96acc8ada61322a93a51.
    
    Revert "sched: Process new forks before processing their parent"
    
    This reverts commit 188c78409d3376ee56780015f5101e15e9f6fe79.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -16,7 +16,7 @@ SCHED_FEAT(START_DEBIT, true)
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, true)
+SCHED_FEAT(NEXT_BUDDY, false)
 
 /*
  * Prefer to schedule the task that ran last (when we did
@@ -29,7 +29,7 @@ SCHED_FEAT(LAST_BUDDY, true)
  * skip buddy i.e task called yield() is always skipped and the
  * next entity is selected to run irrespective of the vruntime
  */
-SCHED_FEAT(STRICT_SKIP_BUDDY, false)
+SCHED_FEAT(STRICT_SKIP_BUDDY, true)
 
 /*
  * Consider buddies to be cache hot, decreases the likelyness of a
@@ -49,13 +49,13 @@ SCHED_FEAT(LB_BIAS, true)
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-SCHED_FEAT(NONTASK_CAPACITY, false)
+SCHED_FEAT(NONTASK_CAPACITY, true)
 
 /*
  * Queue remote wakeups on the target CPU and process them
  * using the scheduler IPI. Reduces rq->lock contention/bounces.
  */
-SCHED_FEAT(TTWU_QUEUE, true)
+SCHED_FEAT(TTWU_QUEUE, false)
 
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
