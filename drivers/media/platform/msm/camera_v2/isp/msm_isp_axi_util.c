KazuDante89/android_kernel_sdm660

msm: camera_v2: conditionality eliminate drop_reconfig flag
The flag is set to notify userspace driver, and expected to be clear
there. However, our userspace driver isn't able to handle this, as a
result the camera stopped working once it's set because an empty frame
is always returned.

This conditionality reverts bc82952
and 39e7df3

Change-Id: I598d8ffc5f2d8eb1fe387a5210e0ef97d2e3badd
Signed-off-by: Hachiman270 <hachiman270@gmail.com>

drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -3628,6 +3628,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		) {
 		pr_debug("%s:%d invalid time to request frame %d try drop_reconfig\n",
 			__func__, __LINE__, frame_id);
+#ifdef CONFIG_MACH_X01BD
+		goto error;
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
 		return 0;
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active) &&
@@ -3642,6 +3645,7 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].active);
 		return 0;
+#endif
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active && (frame_id !=
 		vfe_dev->axi_data.src_info[frame_src].frame_id + vfe_dev->
 		axi_data.src_info[frame_src].sof_counter_step)) ||
@@ -3673,7 +3677,18 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, __LINE__, vfe_dev->pdev->id, frame_id,
 			stream_info->activated_framedrop_period,
 			stream_info->stream_id);
+#ifdef CONFIG_MACH_X01BD
+		rc = msm_isp_return_empty_buffer(vfe_dev, stream_info,
+			user_stream_id, frame_id, buf_index, frame_src);
+		if (rc < 0)
+			pr_err("%s:%d failed: return_empty_buffer src %d\n",
+				__func__, __LINE__, frame_src);
+		stream_info->current_framedrop_period =
+			MSM_VFE_STREAM_STOP_PERIOD;
+		msm_isp_cfg_framedrop_reg(stream_info);
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
+#endif
 		return 0;
 	}
 

zeta96/L_check_msm-4.9

drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -3681,6 +3681,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		) {
 		pr_debug("%s:%d invalid time to request frame %d try drop_reconfig\n",
 			__func__, __LINE__, frame_id);
+#ifdef CONFIG_MACH_XIAOMI_SANTONI
+		goto error;
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
 		do_drop_frame = 1;
 		return 0;
@@ -3697,6 +3700,7 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].active);
 		return 0;
+#endif
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active && (frame_id !=
 		vfe_dev->axi_data.src_info[frame_src].frame_id + vfe_dev->
 		axi_data.src_info[frame_src].sof_counter_step)) ||
@@ -3728,7 +3732,18 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, __LINE__, vfe_dev->pdev->id, frame_id,
 			stream_info->activated_framedrop_period,
 			stream_info->stream_id);
+#ifdef CONFIG_MACH_XIAOMI_SANTONI
+		rc = msm_isp_return_empty_buffer(vfe_dev, stream_info,
+			user_stream_id, frame_id, buf_index, frame_src);
+		if (rc < 0)
+			pr_err("%s:%d failed: return_empty_buffer src %d\n",
+				__func__, __LINE__, frame_src);
+		stream_info->current_framedrop_period =
+			MSM_VFE_STREAM_STOP_PERIOD;
+		msm_isp_cfg_framedrop_reg(stream_info);
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
+#endif
 		return 0;
 	}
 