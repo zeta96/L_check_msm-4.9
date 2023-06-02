f681914e79b8533633db69ccb73e8520531cbd18
zeta96 <ahmad.zainul857@gmail.com>
2023-06-02 20:57:25 +0000

    Revert "fs/exec: fix use after free in execve"
    - unused, fix magisk
    
    This reverts commit d1a33897bb6e150d557cdf045e9f5359483bd18f.

--- fs/exec.c
+++ fs/exec.c
@@ -1706,7 +1706,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-	bool is_su;
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1800,9 +1799,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	/* exec_binprm can release file and it may be freed */
-	is_su = d_is_su(file->f_path.dentry);
-
 	// Super nasty hack to disable lmkd reloading props
 	if (unlikely(strcmp(bprm.filename, "/system/bin/lmkd") == 0)) {
 		if (is_lmkd_reinit(&argv)) {
@@ -1816,7 +1812,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	if (is_su && capable(CAP_SYS_ADMIN)) {
+	if (d_is_su(file->f_path.dentry) && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
 	}
