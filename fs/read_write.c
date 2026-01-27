61735b6b8200dfb000a4a38c2aa53a441a794018
zeta96 <ahmad.zainul857@gmail.com>
2026-01-27 13:55:53 +0000

    Revert "KernelSU: integrate scope-minimized manual hooks v1.4"
    This reverts commit 71ef6e263479c0bdd62223dc3a9ca1815c0d6cac.

--- fs/read_write.c
+++ fs/read_write.c
@@ -581,22 +581,12 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 		file->f_pos = pos;
 }
 
-#ifdef CONFIG_KSU
-extern bool ksu_vfs_read_hook __read_mostly;
-extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
-			size_t *count_ptr);
-#endif
-
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
 	if (f.file) {
-#ifdef CONFIG_KSU
-		if (unlikely(ksu_vfs_read_hook))
-			ksu_handle_sys_read(fd, &buf, &count);
-#endif
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
 		if (ret >= 0)
